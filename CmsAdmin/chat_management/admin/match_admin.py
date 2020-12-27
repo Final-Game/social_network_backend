a3c910ba9bbe8c0b636ff0b06344bd0dbe4cf46e
moonsmile-dev <kakavip.198.kaka@gmail.com>
2020-12-27 18:39:09 +0700

    Update admin for match data.

--- CmsAdmin/chat_management/admin/match_admin.py
+++ CmsAdmin/chat_management/admin/match_admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from core.admin.utils import linkify
 from chat_management.models import Match
+from core.admin.base_admin import BaseAdmin
 
 
 @admin.register(Match)
-class MatchAdmin(admin.ModelAdmin):
+class MatchAdmin(BaseAdmin):
     list_display = [linkify("sender"), linkify("receiver"), "status"]
     list_display_links = []
 
